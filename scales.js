3cf409e72e69dc5fc468e7d3b41b629f9418e682
Igor Josafa Torres Barbosa <igor@Igors-MacBook-Pro.local>
2025-08-08 22:38:34 -0300

    fix: corrige modo eolio

--- scales.js
+++ scales.js
@@ -87,7 +87,7 @@ class mixolydianScale extends sevenNotesScale {
 
 class aeolianScale extends sevenNotesScale {
   constructor(fundamentalNote, octave) {
-    super(fundamentalNote, octave, [0, 2, 4, 5, 7, 9, 11, 12]);
+    super(fundamentalNote, octave, [0, 2, 3, 5, 7, 8, 10, 12]);
   }
 }
 
