0324e4c665824332f62873f98356ee004cb13565
Igor Josafa Torres Barbosa <igor@Mac.bbrouter>
2025-10-10 20:33:28 -0300

    feat: acrescenta exercicio de progressao harmonica

--- games.js
+++ games.js
@@ -13,12 +13,14 @@ class Game {
         this.rodadaAnsweredQuestions = 0;
         this.streak = 0;
         this.longestStreak = 0;
+        this.harmonicProgressionGame = false;
     }
 
     #getRandomNotes(possibleNotes) {
         let leastNote = 0;
         let mostNote = 20;
         let index = Math.floor(Math.random() * possibleNotes.length);
+
         if (index < 7) {
             leastNote = 0;
             mostNote = 6;
@@ -42,6 +44,14 @@ class Game {
             this.notesToGuess.push(possibleNotes[index]);
             this.considerAnswerToScore.push(true);
         }
+
+        // inclui a fundamental se o jogo for o de progressão harmônica
+        if (this.harmonicProgressionGame) {
+            let changeIndex = Math.floor(Math.random() * this.howManyNotes);
+            this.notesIndex[changeIndex] = 0;
+            this.notesToGuess[changeIndex] = possibleNotes[0];
+        }
+
     }
 
     beginRound(possibleNotes) {
@@ -673,4 +683,270 @@ class guessChordNotesGame extends Game {
     
 }
 
-export { guessNoteDegree, guessChordNotesGame }
+class harmonicProgressionGame extends Game {
+  constructor(nameId, title, key, keyFactory) {
+    super(nameId, title);
+    this.keyFactory = keyFactory;
+    this.key = key;
+    this.pureTitle = title;
+    this.howManyNotes = Math.floor(Math.random() * 5) + 3;
+    this.possibleAnswersScore = {
+        'FirstDegree': 0,
+        'SecondDegree': 0,
+        'ThirdDegree': 0,
+        'FourthDegree': 0,
+        'FifthDegree': 0,
+        'SixthDegree': 0,
+        'SeventhDegree': 0
+    };
+    this.possibleAnswersCount = {
+        'FirstDegree': 0,
+        'SecondDegree': 0,
+        'ThirdDegree': 0,
+        'FourthDegree': 0,
+        'FifthDegree': 0,
+        'SixthDegree': 0,
+        'SeventhDegree': 0
+    };
+    this.percentageCorrectAnswers = {
+        'FirstDegree': 0.0,
+        'SecondDegree': 0.0,
+        'ThirdDegree': 0.0,
+        'FourthDegree': 0.0,
+        'FifthDegree': 0.0,
+        'SixthDegree': 0.0,
+        'SeventhDegree': 0.0
+    };
+    this.notesDegrees = [];
+    this.notesToGuess = [];
+    this.notesIndex = [];
+    this.considerAnswerToScore = [];
+    this.noteToCheck = 0;
+    this.correctAnswer = false;
+    this.harmonicProgressionGame = true;
+    this.#updateSelectNoteToCheckElements();
+
+    this.#updateTitleElement();
+  }
+
+  updateNoteToCheck(noteToCheck) {
+    this.noteToCheck = noteToCheck;
+    this.#updateSelectNoteToCheckElements();
+  }
+
+  updateNumberOfNotes(howManyNotes) {
+    this.howManyNotes = howManyNotes;
+    this.#updateSelectNoteToCheckElements();
+  }
+
+  #updateTitleElement() {
+    this.title = this.pureTitle + ` - ${this.key}`;
+    document.getElementById(`${this.nameId}Title`).textContent = this.title;
+  }
+
+  #updateSelectNoteToCheckElements() {
+    const container = document.getElementById(`${this.nameId}SelectNoteToCheck`);
+    container.innerHTML = '';
+    const table = document.createElement("table");
+    table.border = 1;
+    
+    const headerRow = document.createElement("tr");
+    const radioRow = document.createElement("tr");
+
+    for (let i = 0; i < this.howManyNotes; i++) {
+        // cabeçalho
+        const th = document.createElement("th");
+
+        // wrapper flexível
+        const headerContent = document.createElement("div");
+        headerContent.style.display = "flex";
+        headerContent.style.alignItems = "center";
+        headerContent.style.justifyContent = "space-between";
+        headerContent.style.gap = "6px"; // espaço entre texto e botão
+        headerContent.style.whiteSpace = "nowrap"; // evita quebrar linha
+
+        const label = document.createElement("span");
+        label.innerText = `${i + 1}ª Nota`;
+        headerContent.appendChild(label);
+
+        if (i === this.noteToCheck) {
+            const playChordToGuessButton = document.createElement("button");
+            playChordToGuessButton.title = "Tocar o acorde selecionado";
+            playChordToGuessButton.onclick = () => {
+                this.playChordToGuess();
+            };
+
+            const svgPlayChordToGuess = new svgElements(
+                "bi bi-play-fill", 
+                "m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"
+            ).createSvgElement();
+            playChordToGuessButton.appendChild(svgPlayChordToGuess);
+            headerContent.appendChild(playChordToGuessButton);
+
+        }
+
+        th.appendChild(headerContent);
+        headerRow.appendChild(th);
+
+        // input radio
+        const td = document.createElement("td");
+        const radio = document.createElement("input");
+        radio.type = "radio";
+        radio.name = `${this.nameId}NoteToCheck`;
+        radio.id = `${this.nameId}NoteToCheck${i}`;
+        radio.onclick = () => this.updateNoteToCheck(i);
+        if (i === this.noteToCheck) {
+            radio.checked = true;
+        }
+        td.appendChild(radio);
+        radioRow.appendChild(td);
+    }
+
+
+    table.appendChild(headerRow);
+    table.appendChild(radioRow);
+    container.appendChild(table);
+    }
+
+
+  #getAnswerDegrees() {
+    this.notesDegrees = []
+    this.notesIndex.forEach((noteIndex) => {
+        if (noteIndex === 0 || noteIndex === 7 || noteIndex === 14 || noteIndex === 21) {
+            this.notesDegrees = this.notesDegrees.concat(['FirstDegree']);
+        } else if (noteIndex ===  1 || noteIndex === 8 || noteIndex === 15) {
+            this.notesDegrees = this.notesDegrees.concat(['SecondDegree']);
+        } else if (noteIndex === 2 || noteIndex === 9 || noteIndex === 16) {
+            this.notesDegrees = this.notesDegrees.concat(['ThirdDegree']);
+        } else if (noteIndex === 3 || noteIndex === 10 || noteIndex === 17) {
+            this.notesDegrees = this.notesDegrees.concat(['FourthDegree']);
+        } else if (noteIndex === 4 || noteIndex === 11 || noteIndex === 18) {
+            this.notesDegrees = this.notesDegrees.concat(['FifthDegree']);
+        } else if (noteIndex === 5 || noteIndex === 12 || noteIndex === 19) {
+            this.notesDegrees = this.notesDegrees.concat(['SixthDegree']);
+        } else if (noteIndex === 6 || noteIndex === 13 || noteIndex === 20) {
+            this.notesDegrees = this.notesDegrees.concat(['SeventhDegree']);
+        }
+        
+    });
+  }
+
+
+  beginRound() {
+    let possibleKeys = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+    this.key = new this.keyFactory(possibleKeys[Math.floor(Math.random() * possibleKeys.length)], 'ionian', [1, 3, 5], 4)
+    this.title = this.title + ` - ${this.key}`;
+    this.#updateTitleElement();
+    this.howManyNotes = Math.floor(Math.random() * 5) + 3;
+    this.#updateSelectNoteToCheckElements();
+    if (this.notesToGuess.length > 0) {
+        this.#updateScore();
+    }
+    super.beginRound(this.key.extendedScale);
+    this.#getAnswerDegrees();
+    this.playChordsToGuess();
+  }
+
+  checkNoteAnswer(guessedDegree) {
+    this.correctAnswer = (guessedDegree === this.notesDegrees[this.noteToCheck]);
+
+    if (this.considerAnswerToScore[this.noteToCheck]) {
+        this.questions += 1;
+        this.rodadaAnsweredQuestions += 1;
+        this.possibleAnswersCount[this.notesDegrees[this.noteToCheck]] += 1;
+        if (this.correctAnswer) {
+            this.possibleAnswersScore[this.notesDegrees[this.noteToCheck]] += 1;
+            this.acertos += 1;
+        } else {
+            this.allCorrect = false;
+        }
+        this.totalPercentage = this.acertos / this.questions * 100;
+        this.percentageCorrectAnswers[this.notesDegrees[this.noteToCheck]] = (
+            this.possibleAnswersScore[this.notesDegrees[this.noteToCheck]] / this.possibleAnswersCount[this.notesDegrees[this.noteToCheck]]) * 100;
+
+        if (this.rodadaAnsweredQuestions === this.notesToGuess.length) {
+            this.enableNewDraw()
+
+            if (this.allCorrect) {
+                this.rodadasAllCorrect += 1;
+                this.streak += 1;
+                if (this.streak > this.longestStreak) {
+                    this.longestStreak = this.streak;
+                }
+            } else {
+                this.streak = 0;
+            }
+
+            this.showAllCorrect()
+        }
+    }
+
+    this.#showGuessResult();
+    this.showStreak();
+    this.considerAnswerToScore[this.noteToCheck] = false;
+    this.#changeToNextNote();
+
+  }
+
+  #changeToNextNote() {
+    if (this.correctAnswer && this.noteToCheck < this.notesToGuess.length - 1) {
+        document.getElementById(`${this.nameId}NoteToCheck${this.noteToCheck + 1}`).click();
+    } else if (this.correctAnswer) {
+        document.getElementById(`${this.nameId}NoteToCheck0`).click();
+    }
+  }
+
+  #showGuessResult() {
+    if (this.correctAnswer) {
+        document.getElementById(`${this.nameId}GuessedAnswer`).textContent = `Resposta Correta! A nota sorteada foi ${this.notesToGuess[this.noteToCheck].name}.`;
+    } else {
+        document.getElementById(`${this.nameId}GuessedAnswer`).textContent = `Resposta Errada!`;
+    }
+  }
+
+  #updateScore() {
+    this.notesDegrees.forEach((degree) => {
+        document.getElementById(`${this.nameId}${degree}Score`).textContent = this.possibleAnswersScore[degree];
+        document.getElementById(`${this.nameId}${degree}Total`).textContent = this.possibleAnswersCount[degree];
+        document.getElementById(`${this.nameId}${degree}Percentual`).textContent = this.percentageCorrectAnswers[degree].toFixed(2) + '%';
+
+        document.getElementById(`${this.nameId}TotalScore`).textContent = this.acertos;
+        document.getElementById(`${this.nameId}TotalGuesses`).textContent = this.questions;
+        document.getElementById(`${this.nameId}TotalPercentual`).textContent = this.totalPercentage.toFixed(2) + '%';
+    });
+    
+  }
+
+    playBaseChord() {
+        const duration = parseFloat(document.getElementById(`${this.nameId}DurationPerChord`).value);
+        const volume = parseFloat(document.getElementById('volume').value)/100;
+        this.key.harmonicField.chords[0].play(window.player, volume, duration);
+        }
+
+  playChordToGuess() {
+      const duration = parseFloat(document.getElementById(`${this.nameId}DurationPerChord`).value);
+      const volume = parseFloat(document.getElementById('volume').value)/100;
+
+      this.key.harmonicField.chords.forEach(element => {
+        if (this.notesToGuess[this.noteToCheck].name === element.notes[0].name) {
+          element.play(window.player, volume, duration);
+        }
+      });
+  }
+
+  playChordsToGuess() {
+      const duration = parseFloat(document.getElementById(`${this.nameId}DurationPerChord`).value);
+      const volume = parseFloat(document.getElementById('volume').value)/100;
+
+      this.notesIndex.forEach((index, time) => {
+        setTimeout(() => {
+            this.key.harmonicField.chords[index % 7].play(window.player, volume, duration);
+        }, time * duration * 1000);
+      });
+  }
+
+}
+
+export { guessNoteDegree, guessChordNotesGame, harmonicProgressionGame }
+
+
